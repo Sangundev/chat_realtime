607021f459c5cd153de06d74b88ed97437c02899
Sang221102 <nguyenngocsang1682@gmail.com>
2024-04-03 18:44:08 +0700

    ux/ui signup and logout

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -2,53 +2,55 @@ import bcrypt from "bcryptjs";
 import User from "../models/user.model.js";
 import generateTokenAndSetCookie from "../utils/generateToken.js";
 
-export const singup = async (req, res) => {
-  try {
-    const { fullname, username, password, comfirmPassword, gender } = req.body;
+export const signup = async (req, res) => {
+	try {
+		const { fullName, username, password, confirmPassword, gender } = req.body;
 
-    if (password !== comfirmPassword) {
-      return res.status(400).json({ error: "khong khop mk " });
-    }
+		if (password !== confirmPassword) {
+			return res.status(400).json({ error: "Passwords don't match" });
+		}
 
-    const user = await User.findOne({ username });
+		const user = await User.findOne({ username });
 
-    if (user) {
-      return res.status(400).json({ error: ______ });
-    }
+		if (user) {
+			return res.status(400).json({ error: "Username already exists" });
+		}
 
-    // hash password
-    const salt = await bcrypt.genSalt(10);
-    const hashpashh = await bcrypt.hash(password, salt);
+		// HASH PASSWORD HERE
+		const salt = await bcrypt.genSalt(10);
+		const hashedPassword = await bcrypt.hash(password, salt);
 
-    // https://avatar.iran.liara.run/public
-    const boypic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
-    const girlpic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+		// https://avatar-placeholder.iran.liara.run/
 
-    const newUser = new User({
-      fullname,
-      username,
-      password: hashpashh,
-      gender,
-      profilePic: gender === "male" ? boypic : girlpic,
-    });
+		const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+		const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
 
-    if (newUser) {
-      //generate JWT
-      generateTokenAndSetCookie(newUser._id, res);
-      await newUser.save();
-      res.status(201).json({
-        _id: newUser._id,
-        fullname: newUser.fullname,
-        username: newUser.username,
-        profilePic: newUser.profilePic,
-      });
-    } else {
-      res.status(400).json({ error: "loi khi tao user trong data" });
-    }
-  } catch (error) {
-    console.log("lois khi tao tai khoan ", error.message);
-    res.status(500).json({ error: "server Erroooo" });
-  }
+		const newUser = new User({
+			fullName,
+			username,
+			password: hashedPassword,
+			gender,
+			profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
+		});
+
+		if (newUser) {
+			// Generate JWT token here
+			generateTokenAndSetCookie(newUser._id, res);
+			await newUser.save();
+
+			res.status(201).json({
+				_id: newUser._id,
+				fullName: newUser.fullName,
+				username: newUser.username,
+				profilePic: newUser.profilePic,
+			});
+		} else {
+			res.status(400).json({ error: "Invalid user data" });
+		}
+	} catch (error) {
+		console.log("Error in signup controller", error.message);
+		res.status(500).json({ error: "Internal Server Error" });
+	}
 };
 
 export const login = async (req, res) => {
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,22 @@
+import { Navigate, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Home from './page/home/Home'
-// import Login from './page/login/Login'
-// import Signup from './page/signup/Signup'
+import Login from './page/login/Login'
+import Signup from './page/signup/Signup'
+import { Toaster } from 'react-hot-toast'
+import { useAuthContext } from './context/AuthContext'
 
 
 function App() {
-
+  const {authUser} = useAuthContext();
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
-      {/* <Signup /> */}
-      <Home />
+      <Routes>
+      <Route path='/' element={authUser ? <Home />: <Navigate to={"/Login"} />  } />
+      <Route path='/login' element={authUser ? <Navigate to='/' /> : <Login />} />
+      <Route path='/signup' element={authUser ? <Navigate to='/' /> : <Signup />} />
+      </Routes>
+      <Toaster />
     </div>
   )
 }
