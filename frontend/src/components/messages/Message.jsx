9f68446f8511e72013eb7635c7037c657f18abd6
Sang221102 <nguyenngocsang1682@gmail.com>
2024-04-04 19:00:41 +0700

    update-chat

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -1,16 +1,28 @@
-const Message = () => {
-  return (
-   <div className="chat chat-end">
-        <div className="chat-image avatar">
-            <div className="w-10 rounded-full">
-                <img src="https://cdn.dxomark.com/wp-content/uploads/medias/post-
-                157904/Apple-iPhone-15_-blue_featured-image-packshot-review.jpg" alt="User Avatar" />
-            </div>
-        </div>
-        <div className={`chat-bubble text-white pb-2`}>Hi hi hi hi </div>
-			<div className='chat-footer opacity-50 text-xs flex gap-1 items-center'>12:42</div>
-   </div>
-  )
-}
+import { useAuthContext } from "../../context/AuthContext";
+import { extractTime } from "../../utils/extractTime";
+import useConversation from "../../zustand/useConversation";
 
-export default Message
+const Message = ({ message }) => {
+	const { authUser } = useAuthContext();
+	const { selectedConversation } = useConversation();
+	const fromMe = message.senderId === authUser._id;
+	const formattedTime = extractTime(message.createdAt);
+	const chatClassName = fromMe ? "chat-end" : "chat-start";
+	const profilePic = fromMe ? authUser.profilePic : selectedConversation?.profilePic;
+	const bubbleBgColor = fromMe ? "bg-blue-500" : "";
+
+	const shakeClass = message.shouldShake ? "shake" : "";
+
+	return (
+		<div className={`chat ${chatClassName}`}>
+			<div className='chat-image avatar'>
+				<div className='w-10 rounded-full'>
+					<img alt='Tailwind CSS chat bubble component' src={profilePic} />
+				</div>
+			</div>
+			<div className={`chat-bubble text-white ${bubbleBgColor} ${shakeClass} pb-2`}>{message.message}</div>
+			<div className='chat-footer opacity-50 text-xs flex gap-1 items-center'>{formattedTime}</div>
+		</div>
+	);
+};
+export default Message;
--- frontend/src/page/login/Login.jsx
+++ frontend/src/page/login/Login.jsx
@@ -1,7 +1,20 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import useLogin from "../../hooks/useLogin";
 
 
 const Login = () => {
+
+  const [username,setUsername] = useState("");
+  const [password,setpassword] = useState("");
+  
+  const {loading,login} = useLogin();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await login(username,password);
+  };
+
   return (
     <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
     <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
@@ -10,7 +23,7 @@ const Login = () => {
         <span className='text-blue-500'> ChatApp</span>
       </h1>
 
-      <form >
+      <form onSubmit={handleSubmit}>
         <div>
           <label className='label p-2'>
             <span className='text-base label-text'>Username</span>
@@ -19,6 +32,9 @@ const Login = () => {
             type='text'
             placeholder='Enter username'
             className='w-full input input-bordered h-10'
+
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
           />
         </div>
 
@@ -30,6 +46,9 @@ const Login = () => {
             type='password'
             placeholder='Enter Password'
             className='w-full input input-bordered h-10'
+
+            value={password}
+            onChange={(e) => setpassword(e.target.value)}
           />
         </div>
         <Link to="/signup" className='text-sm  hover:underline hover:text-blue-600 mt-2 inline-block'>
@@ -37,8 +56,9 @@ const Login = () => {
         </Link>
 
         <div>
-          <button className='btn btn-block btn-sm mt-2'>
-            Login
+          <button className='btn btn-block btn-sm mt-2'
+          disabled={loading} >
+            {loading ? <span className="loading loading-spinner" ></span> : "Login"}
           </button>
         </div>
       </form>
